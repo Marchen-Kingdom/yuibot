54251e0d1a31d685762c741499b23b4a0d4821d2
Kaitian Xie <kaitian96@outlook.com>
2020-12-27 19:40:31 +0000

    Remove download_go-cqhttp.sh

--- scripts/download_go-cqhttp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -r go-cqhttp
-mkdir go-cqhttp
-cd go-cqhttp
-curl -LJO https://github.com/Mrs4s/go-cqhttp/releases/download/v0.9.35-fix1/go-cqhttp-v0.9.35-fix1-linux-amd64.tar.gz
-tar -xvzf go-cqhttp-v0.9.35-fix1-linux-amd64.tar.gz
-rm go-cqhttp-v0.9.35-fix1-linux-amd64.tar.gz
